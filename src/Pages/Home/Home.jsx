69caa18c3857e99f90513e6f1e9cc0cc72ccc1b9
millamzy <ndefoago@gmail.com>
2024-07-10 13:56:38 +0100

    commit forth

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../../components/Navbar/Navbar.jsx';
+import Navbar from '../../components/Navbar/Navbar.jsx'
 import Hero from '../../components/Hero/Hero.jsx';
 import Card from '../../components/Card/Card.jsx';
 import Footer from '../../components/Footer/Footer.jsx';
